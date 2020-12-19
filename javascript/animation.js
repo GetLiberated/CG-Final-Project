ddd9eea94460ef6ef4ce9f4e6975741946481929
GetLiberated <43334192+GetLiberated@users.noreply.github.com>
2020-12-20 06:30:18 +0700

    Level 3 load map and cutscene. Improved stability

--- javascript/animation.js
+++ javascript/animation.js
@@ -375,3 +375,207 @@ function cutscene2Animation(frameRate) {
 
     return [camera_rotate, camera_position, car_move, fadeout]
 }
+
+function cutscene3Animation(frameRate) {
+    var camera_rotate = new BABYLON.Animation("camera_rotate", "rotation", frameRate, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+                        
+    var camera_rotate_keys = []; 
+
+    // scene 1 - street panoramic view
+    camera_rotate_keys.push({
+        frame: 2,
+        value: new BABYLON.Vector3(0.1, -2.2, 0)
+    });
+
+    camera_rotate_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Vector3(0.1, -0.9, 0)
+    });
+
+
+
+
+    // scene 2.0
+    camera_rotate_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Vector3(0.5, -1.57, 0)
+    });
+
+    camera_rotate_keys.push({
+        frame: 10 * frameRate,
+        value: new BABYLON.Vector3(0.4, -1.57, 0)
+    });
+
+
+
+
+    // scene 2.1
+    camera_rotate_keys.push({
+        frame: 12 * frameRate, 
+        value: new BABYLON.Vector3(-0.02, -1.57, 0)
+    });
+
+    camera_rotate_keys.push({
+        frame: 13 * frameRate,
+        value: new BABYLON.Vector3(-0.02, -0.75, 0)
+    });
+
+    camera_rotate_keys.push({
+        frame: 15 * frameRate,
+        value: new BABYLON.Vector3(0.1, -1.57, 0)
+    });
+
+
+
+
+    // scene 2.2
+    camera_rotate_keys.push({
+        frame: 17 * frameRate,
+        value: new BABYLON.Vector3(0.34, -1.57, 0)
+    });
+
+    camera_rotate_keys.push({
+        frame: 18 * frameRate,
+        value: new BABYLON.Vector3(-0.04, 0, 0)
+    });
+
+    camera_rotate_keys.push({
+        frame: 19 * frameRate,
+        value: new BABYLON.Vector3(-0.01, 1.57, 0)
+    });
+
+
+
+
+    camera_rotate.setKeys(camera_rotate_keys);
+    
+    var camera_position = new BABYLON.Animation("camera_position", "position", frameRate, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+    
+    var camera_position_keys = []; 
+
+    // scene 1 - street panoramic view
+    camera_position_keys.push({
+        frame: 2,
+        value: new BABYLON.Vector3(98.3, 27.2, 30.3)
+    });
+
+    camera_position_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Vector3(107.9, 26, -180)
+    });
+
+
+
+
+    // scene 2.0 - turun
+    camera_position_keys.push({
+        frame: 8 * frameRate,
+        value: new BABYLON.Vector3(117, 70, -70)
+    });
+
+    camera_position_keys.push({
+        frame: 10 * frameRate,
+        value: new BABYLON.Vector3(46, 10, -70)
+    });
+
+
+
+
+    // scene 2.1 - masuk
+    camera_position_keys.push({
+        frame: 12 * frameRate,
+        value: new BABYLON.Vector3(2.4, 3.65, -70)
+    });
+
+    camera_position_keys.push({
+        frame: 13 * frameRate,
+        value: new BABYLON.Vector3(2.4, 3.65, -70)
+    });
+
+    camera_position_keys.push({ // menuju turun
+        frame: 15 * frameRate,
+        value: new BABYLON.Vector3(-6, 7.5, -38.5)
+    });
+
+
+
+
+    // scene 2.2 - turun
+    camera_position_keys.push({
+        frame: 17 * frameRate,
+        value: new BABYLON.Vector3(-64, -1, -38.5)
+    });
+
+    camera_position_keys.push({ // kanan
+        frame: 18 * frameRate,
+        value: new BABYLON.Vector3(-90.3, -6.4, -23.2)
+    });
+
+    camera_position_keys.push({ // kanan
+        frame: 19 * frameRate,
+        value: new BABYLON.Vector3(-90.3, -6.4, -19)
+    });
+
+    camera_position_keys.push({ // stay
+        frame: 20 * frameRate,
+        value: new BABYLON.Vector3(-90.3, -6.4, -19)
+    });
+
+    camera_position_keys.push({ // trigger
+        frame: 21 * frameRate,
+        value: new BABYLON.Vector3(-100, -6.4, -19)
+    });
+
+
+
+
+    camera_position.setKeys(camera_position_keys);
+
+    var fadeout = new BABYLON.Animation("fadeout", "alpha", frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+    
+    var fadeout_keys = []; 
+
+    fadeout_keys.push({
+        frame: 0,
+        value: 1
+    });
+
+    fadeout_keys.push({
+        frame: 1 * frameRate,
+        value: 1
+    });
+
+    fadeout_keys.push({
+        frame: 2 * frameRate,
+        value: 0
+    });
+
+    fadeout_keys.push({
+        frame: 7 * frameRate,
+        value: 0
+    });
+
+    fadeout_keys.push({
+        frame: 8 * frameRate,
+        value: 1
+    });
+
+    fadeout_keys.push({
+        frame: 9 * frameRate,
+        value: 0
+    });
+
+    fadeout_keys.push({
+        frame: 19 * frameRate,
+        value: 0
+    });
+
+    fadeout_keys.push({
+        frame: 20 * frameRate,
+        value: 1
+    });
+
+    fadeout.setKeys(fadeout_keys);
+
+    return [camera_rotate, camera_position, fadeout]
+}
--- javascript/map.js
+++ javascript/map.js
@@ -1,6 +1,8 @@
 /*****************************Map********************************************/
 
 var mapMeshes;
+var roadMeshes;
+var trafficLightMeshes;
 var trafficLight = []
 var persons = []
 var personsAnimation = []
@@ -19,6 +21,7 @@ function loadMap(scene) {
     });
     
     new BABYLON.SceneLoader.ImportMesh(null, "./assets/Environments/", "Road.obj", scene, (meshes) => {
+        roadMeshes = meshes
         meshes.forEach((mesh, index) => {
             mesh.position = new BABYLON.Vector3(0, 0, 0)
             mesh.scaling = new BABYLON.Vector3(3.2, 3.2, 3.2);
@@ -31,6 +34,7 @@ function loadMap(scene) {
     });
     
     new BABYLON.SceneLoader.ImportMesh(null, "./assets/Environments/Traffic Light/", "traffic light.obj", scene, (meshes) => {
+        trafficLightMeshes = meshes
         meshes.forEach((mesh, index) => {
             mesh.position = new BABYLON.Vector3(-127, 0, 40)
             mesh.scaling = new BABYLON.Vector3(0.4, 0.4, 0.4);
@@ -113,65 +117,32 @@ function loadMap(scene) {
 }
 
 var mapMeshes2;
-function loadMap2(scene, car, fail, lose) {
+var roadMeshes2;
+function loadMap2(scene) {
 
     new BABYLON.SceneLoader.ImportMesh(null, "./assets/Environments/", "MallEnvironment.obj", scene, (meshes) => {
-        mapMeshes = meshes
+        mapMeshes2 = meshes
         meshes.forEach((mesh, index) => {
             mesh.position = new BABYLON.Vector3(0, 0, 0)
             mesh.scaling = new BABYLON.Vector3(3.2, 3.2, 3.2);
             mesh.rotation = new BABYLON.Vector3(0, 0, 0)
             // mesh.showBoundingBox = true;
-            mesh.checkCollisions = true
-            mesh.physicsImpostor = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
+            // mesh.checkCollisions = true
+            mesh.setEnabled(false)
         });
     });
     
     new BABYLON.SceneLoader.ImportMesh(null, "./assets/Environments/", "MallRoad.obj", scene, (meshes) => {
+        roadMeshes2 = meshes
         meshes.forEach((mesh, index) => {
             mesh.position = new BABYLON.Vector3(0, 0, 0)
             mesh.scaling = new BABYLON.Vector3(3.2, 3.2, 3.2);
             mesh.rotation = new BABYLON.Vector3(0, 0, 0)
             // mesh.showBoundingBox = true;
-            mesh.checkCollisions = true
-            mesh.physicsImpostor = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0 , friction: 0.8}, scene);
+            // mesh.checkCollisions = true
+            mesh.setEnabled(false)
         });
     });
-    
-    var boundary = BABYLON.Mesh.CreatePlane("", 384)
-    boundary.rotation.y = -Math.PI/2
-    boundary.position = new BABYLON.Vector3(-192, 0, 0)
-    var mat = new BABYLON.StandardMaterial()
-    mat.diffuseColor = new BABYLON.Color3(0, 0, 0)
-    mat.alpha = 0
-    boundary.material = mat
-    boundary.physicsImpostor = new BABYLON.PhysicsImpostor(boundary, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
-    
-    var leftBoundary = BABYLON.Mesh.CreatePlane("", 384)
-    leftBoundary.rotation.y = Math.PI
-    leftBoundary.position = new BABYLON.Vector3(0, 0, -230)
-    var mat = new BABYLON.StandardMaterial()
-    mat.diffuseColor = new BABYLON.Color3(0, 0, 0)
-    mat.alpha = 0
-    leftBoundary.material = mat
-    leftBoundary.physicsImpostor = new BABYLON.PhysicsImpostor(leftBoundary, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
-
-    var rightBoundary = BABYLON.Mesh.CreatePlane("", 384)
-    rightBoundary.position = new BABYLON.Vector3(0, 0, 230)
-    var mat = new BABYLON.StandardMaterial()
-    mat.diffuseColor = new BABYLON.Color3(0, 0, 0)
-    mat.alpha = 0
-    rightBoundary.material = mat
-    rightBoundary.physicsImpostor = new BABYLON.PhysicsImpostor(rightBoundary, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
-
-    var backBoundary = BABYLON.Mesh.CreatePlane("", 384)
-    backBoundary.rotation.y = Math.PI/2
-    backBoundary.position = new BABYLON.Vector3(192, 0, 0)
-    var mat = new BABYLON.StandardMaterial()
-    mat.diffuseColor = new BABYLON.Color3(0, 0, 0)
-    mat.alpha = 0
-    backBoundary.material = mat
-    backBoundary.physicsImpostor = new BABYLON.PhysicsImpostor(backBoundary, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0}, scene);
 }
 
 
